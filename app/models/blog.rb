d78f2ac136add652541342f6e5a03e0d9049ebc5
nosolopau <pau@nosolopau.com>
2015-09-06 22:24:12 +0200

    Basic application

---
+++ app/models/blog.rb
@@ -0,0 +1,9 @@
+class Blog
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+
+  has_many :posts
+  belongs_to :user
+end
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :content, type: String
+
+  belongs_to :blog
+end
